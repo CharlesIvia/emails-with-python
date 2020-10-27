78ed6180a6622113cf214c406a141cce538436e7
Charles Ivia <iviacharles@gmail.com>
2020-10-27 13:36:47 +0300

    Sending email to multiple contacts

--- mail_script.py
+++ mail_script.py
@@ -7,16 +7,20 @@ from email.message import EmailMessage
 EMAIL_ADDRESS = os.environ.get("TEST_EMAIL")
 EMAIL_PASSWORD = os.environ.get("TEST_PASS")
 
+# Contact list
+
+contacts = ["charlestitanmarket@gmail.com", "iviacharles@gmail.com"]
+
 # Create email
 
 msg = EmailMessage()
 msg["Subject"] = "Test Drive this weekend"
 msg["From"] = EMAIL_ADDRESS
-msg["To"] = "charlestitanmarket@gmail.com"
+msg["To"] = contacts
 msg.set_content("Hello, The car is ready for a test drive whenever you are.")
 
 # connect to smtp mail server
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login(EMAIL_ADDRESS, "Xanadu2020")
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     smtp.send_message(msg)
